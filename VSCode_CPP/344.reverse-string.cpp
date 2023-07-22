215bc07f4e6b459433c7d68197e86e140921686e
Yuchen Liu <yuchen.liu@memverge.com>
2023-07-22 12:02:48 -0700

    Sat Jul 22 12:02:48 PDT 2023

--- VSCode_CPP/344.reverse-string.cpp
+++ VSCode_CPP/344.reverse-string.cpp
@@ -8,10 +8,7 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n = s.size();
-        for (int i=0; i<n/2; i++) {
-            swap(s[i], s[n-i-1]);
-        }
+        reverse(s.begin(), s.end());
     }
 };
 // @lc code=end
